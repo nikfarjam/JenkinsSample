0932522f4b717f296291167a0d6e211d4297ea13
nikfarjam <mm.nikfarjam@gmail.com>
2016-01-20 12:59:45 +1100

    add a bug on purpose

--- src/main/java/StringUtilImpl.java
+++ src/main/java/StringUtilImpl.java
@@ -4,7 +4,7 @@
 public class StringUtilImpl implements StringUtil {
     public boolean isEmpty(String str) {
         if (str == null) {
-            return true;
+            return false;
         }
         if (str.length() == 0) {
             return true;
